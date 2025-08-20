7401483fced28d53cc71e3c26d547f7d10c4358d
NataliaBN <natibn@outlook.com.br>
2025-08-20 08:43:03 -0300

    GET games by list

--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,9 +24,15 @@ public class GameService {
         return new GameDTO(result);
     }
 
-   @Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(GameMinDTO::new).toList();
     }
 }
